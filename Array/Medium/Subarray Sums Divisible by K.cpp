c85832f937493ddfd180fa7ddc8b23c63b518613
Miraj Asraf <75885581+Miraj8280@users.noreply.github.com>
2022-05-12 18:24:16 +0530

    Update Subarray Sums Divisible by K.cpp

--- Array/Medium/Subarray Sums Divisible by K.cpp
+++ Array/Medium/Subarray Sums Divisible by K.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int curr = 0, count = 0;
+        unordered_map<int, int> um = {{0, 1}};
+        
+        for(int i = 0; i < nums.size(); ++i){
+            curr = (curr + nums[i] % k + k) % k;
+            count += um[curr];
+            um[curr]++;
+        }
+        
+        return count;
+    }
+};
