abf36d59a451d47fc9662a14a7e43b1d4df370da
Miraj Asraf <75885581+Miraj8280@users.noreply.github.com>
2022-05-25 16:24:13 +0530

    Create Valid Parentheses.cpp

---
+++ String/Easy/Valid Parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(auto i : s){
+            if(i == '(' || i == '{' || i == '[') stk.push(i);
+            else{
+                if(stk.empty() || (stk.top() == '(' && i != ')') || (stk.top() == '{' && i != '}') || (stk.top() == '[' && i != ']')) return false;
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
